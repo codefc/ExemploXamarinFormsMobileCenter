1932fa28f1cf46fc7c420b6356f12c76b00dc69c
Albert Tanure <tanure@live.com>
2017-08-24 18:19:33 -0300

    Primeria versão do código fonte

---
+++ ExemploAplicacaoMobileCenter/ExemploAplicacaoMobileCenter/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace ExemploAplicacaoMobileCenter.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExemploAplicacaoMobileCenter/ExemploAplicacaoMobileCenter/Views/ItemsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using ExemploAplicacaoMobileCenter.Models;
+using ExemploAplicacaoMobileCenter.ViewModels;
+
+using Xamarin.Forms;
+
+namespace ExemploAplicacaoMobileCenter.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+
+    }
+}
